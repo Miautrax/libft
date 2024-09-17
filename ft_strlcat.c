603812017cd6fba794fd6b8a4fe77a0ec89aaeff
Miautrax <rvadri.9.12@gmail.com>
2024-09-17 19:03:44 +0000

    ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arivas-q <arivas-q@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 18:49:09 by arivas-q          #+#    #+#             */
+/*   Updated: 2024/09/17 18:49:20 by arivas-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+size_t strlcat(char *dest, const char *src, size_t size)
+{
+    size_t  i;
+
+    i = 0;
+
+    se puede usar la función de la librería ft_strlen?
+}
