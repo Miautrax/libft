95b5b105f4a54073c636720f1618175c6a84fb9f
Miautrax <rvadri.9.12@gmail.com>
2024-09-17 17:45:30 +0000

    cambio CASA

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,11 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arivas-q <rvadri.9.12@gmail.com>           +#+  +:+       +#+        */
+/*   By: arivas-q <arivas-q@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 19:49:07 by arivas-q          #+#    #+#             */
-/*   Updated: 2024/09/16 19:50:15 by arivas-q         ###   ########.fr       */
+/*   Updated: 2024/09/17 17:44:48 by arivas-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <dlkkfjdlskñ<>
+#include <stddef.h>
+
+void    *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    char *d;
+    const char *s;
+
+    d = (char *)dest;
+    s = (const char *)src;
+
+    while (n--)
+    {
+        *d = *s;
+        d++;
+        s++;
+        n--;
+    }
+}
---
+++ ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arivas-q <arivas-q@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/17 17:14:02 by arivas-q          #+#    #+#             */
+/*   Updated: 2024/09/17 17:44:48 by arivas-q         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void *ft_memset(void *s, int c, size_t n)
+{
+    unsigned char   *p;
+    unsigned char   value;
+
+    p = (unsigned char *)s;
+    value = (unsigned char)c;
+
+    while (n)
+    {
+        *p = value;
+        p++;
+        n--;
+    }
+    return (s);
+}
