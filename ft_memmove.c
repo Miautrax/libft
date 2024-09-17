bd64b3247ef870925e4bc32673a6bcb02969172a
Miautrax <rvadri.9.12@gmail.com>
2024-09-17 18:29:12 +0000

    yes

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,8 +6,35 @@
 /*   By: arivas-q <arivas-q@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:37:07 by arivas-q          #+#    #+#             */
-/*   Updated: 2024/09/17 17:44:48 by arivas-q         ###   ########.fr       */
+/*   Updated: 2024/09/17 17:57:57 by arivas-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char   *d;
+    const unsigned char *s;
+
+    d = (unsigned char *)dest;
+    s = (const unsigned char *)src;
+    
+    if (d == s)
+        return (dest);
+
+    if (d < s && d + n > s)
+    {
+        d = d + n;
+        s = s + n;
+        while (n--)
+            *--d = *--s;
+    }
+    else
+    {
+        while (n--)
+            *d++ = *s++;
+    }
+
+    return (dest);
+}
