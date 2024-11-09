7b0cf849ba4971dfa870c6e80669442d687e4938
Miautrax <rvadri.9.12@gmail.com>
2024-11-09 15:05:19 +0000

    yes

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: arivas-q <arivas-q@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:59:14 by arivas-q          #+#    #+#             */
-/*   Updated: 2024/09/24 16:22:44 by arivas-q         ###   ########.fr       */
+/*   Updated: 2024/11/09 15:03:34 by arivas-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	i;
+	size_t	j;
 	
+	i = 0;
+	
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && (i + j) < len)
+		{
+			j++;
+			if (needle[j] == '\0')
+				return (char *)(haystack + i);
+		}
+		i++;
+	}
+	return (NULL);
 }
