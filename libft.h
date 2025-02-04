06747370019863562d2288798f5fb1d5b7cc5ed8
Miautrax <rvadri.9.12@gmail.com>
2025-02-04 08:52:06 +0000

    r

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: arivas-q <arivas-q@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 13:42:24 by arivas-q          #+#    #+#             */
-/*   Updated: 2025/02/04 08:38:32 by arivas-q         ###   ########.fr       */
+/*   Updated: 2025/02/04 08:51:55 by arivas-q         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int		ft_isdigit(int number);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int number);
-size_t	ft_strlen(char	*str);
+size_t	ft_strlen(const char	*str);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
